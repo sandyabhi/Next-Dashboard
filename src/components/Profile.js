e8d77fe75071a622875e405e7df42eca43952e6e
Sandeep Kumar Patel <abhisandy47@gmail.com>
2023-10-13 16:48:57 +0530

    sign out

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,14 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import React, { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Profile = () => {
+  const [visible, setVisible] = useState(false);
+
+  const router = useRouter();
   const session = useSession();
 
   const User = {
@@ -12,12 +17,39 @@ const Profile = () => {
     img: session?.data?.user.image,
   };
 
-  //   console.log(session);
+  function handleSignout() {
+    signOut();
+
+    router.push("/");
+  }
 
   return (
-    <div className="bg-slate-400 rounded-[50%]">
-      <img alt="profile_img" className="w-10 rounded-[50%]" src={User.img} />
-    </div>
+    <>
+      <div className="bg-slate-400 rounded-[50%] relative">
+        <img
+          onClick={() => {
+            console.log(visible);
+            setVisible(!visible);
+          }}
+          alt="img"
+          className="w-10 rounded-[50%]"
+          src={User.img}
+        />
+      </div>
+      {visible && (
+        <div className="absolute ml-4 mt-24 z-50 py-2 w-16 text-sm text-black bg-white shadow-md rounded-lg flex flex-col justify-center items-center">
+          <div className="hover:bg-slate-100 px-3 w-full ">
+            <Link href="/">Home</Link>
+          </div>
+          <div
+            onClick={() => handleSignout()}
+            className="hover:bg-slate-100 px-3 w-full cursor-pointer"
+          >
+            Logout
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
